6b1c8003ff4a53a569c3931020c9b75c9957e56f
Soyombo Ifeoluwa <108593370+Soyombo-Ifeoluwa@users.noreply.github.com>
2022-11-06 14:45:54 +0100

    React

--- src/NestedNav/RoutesNav.js
+++ src/NestedNav/RoutesNav.js
@@ -1,16 +1,25 @@
 import { NavLink } from "react-router-dom";
 
-
 const RouteNav = () => {
-  return(
-   <div className="nested-bar">
-      <NavLink to="shoe" className="nested-nav">Shoes</NavLink>
-      <NavLink to="skirts" className="nested-nav">Skirts</NavLink>
-      <NavLink to="shirts" className="nested-nav">Shirts</NavLink>
-      <NavLink to="trousers" className="nested-nav" >Trousers</NavLink>
-      <NavLink to="hoodies" className="nested-nav">Hoodies</NavLink>
-   </div>
-  )
-}
+  return (
+    <div className="nested-bar">
+      <NavLink to="shoe" className="nested-nav">
+        Shoes
+      </NavLink>
+      <NavLink to="skirts" className="nested-nav">
+        Skirts
+      </NavLink>
+      <NavLink to="shirts" className="nested-nav">
+        Shirts
+      </NavLink>
+      <NavLink to="trousers" className="nested-nav">
+        Trousers
+      </NavLink>
+      <NavLink to="hoodies" className="nested-nav">
+        Hoodies
+      </NavLink>
+    </div>
+  );
+};
 
 export default RouteNav;
